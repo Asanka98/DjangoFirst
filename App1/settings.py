564b71ce558edf2943c9e29a895c08bc84355084
Asanka <asanka.asb12345@gmail.com>
2022-08-15 17:33:33 +0530

    IP Update

--- App1/settings.py
+++ App1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dhw%x%enp(=%o*wgwtekf!g1%4o$qg4)tafvv+^+-#58+^sw)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
